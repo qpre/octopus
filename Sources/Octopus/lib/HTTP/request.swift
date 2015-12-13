93861bea000d230bd260a4150cfd7f7b62636949
Quentin Pre <pre.quentin@gmail.com>
2015-12-13 23:42:07 +0100

    [UPD] HTTPMethod as public

---
+++ Sources/Octopus/lib/HTTP/request.swift
@@ -0,0 +1,56 @@
+public enum HTTPMethod {
+  case GET
+  case POST
+}
+
+public struct HTTPRequest {
+  var method:  HTTPMethod
+  var uri:     String = ""
+  var version: String = ""
+}
+
+public enum HTTPRequestError: ErrorType {
+  case BadRequest
+  case NotSupported
+}
+
+public func getHTTPRequestErrorParams(error: HTTPRequestError) -> (Int, String) {
+  switch error {
+    case .BadRequest:
+      return (code: 400, message: "Bad Request")
+    case .NotSupported:
+      return (code: 505, message: "HTTP Version Not Supported")
+  }
+}
+
+public func parseRequest(requestAsString: String) throws -> HTTPRequest {
+  var request: HTTPRequest
+  let params = requestAsString.characters.split{$0 == " "}.map(String.init)
+
+  if (params.count < 3) {
+    throw HTTPRequestError.BadRequest
+  }
+
+  request = HTTPRequest(
+    method: try! parseMethod(params[0]),
+    uri: params[1],
+    version: params[2]
+  )
+
+  return request
+}
+
+func parseMethod(string: String) throws -> HTTPMethod {
+  let method: HTTPMethod
+
+  switch string.uppercaseString {
+    case "GET":
+      method = HTTPMethod.GET
+    case "POST":
+      method = HTTPMethod.POST
+    default:
+      throw HTTPRequestError.NotSupported
+  }
+
+  return method
+}
---
+++ Sources/Octopus/lib/server.swift
@@ -0,0 +1,81 @@
+/*
+** server.swift
+*/
+
+// System specific imports
+#if os(Linux)
+  import Glibc
+#else
+  import Foundation
+#endif
+
+func sync(lock: AnyObject, closure: () -> Void) {
+  objc_sync_enter(lock)
+  closure()
+  objc_sync_exit(lock)
+}
+
+public class OctopusServer {
+  var socket:  OctopusSocket
+  var clients: Set<OctopusSocket>
+
+  public init(port: in_port_t = 8080) {
+    self.socket  = try! createSocket(port)
+    self.clients = Set<OctopusSocket>()
+  }
+
+  public func start() throws {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+      while let client = try? acceptClientSocket(self.socket) {
+        sync (self) {
+          self.clients.insert(client)
+        }
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+          // let address = try! getPeerName(client)
+
+          let requestString = try? readSocket(client)
+
+          do {
+            let request = try parseRequest(requestString!)
+          } catch let e {
+            let error = e as? HTTPRequestError
+            let (code, message) = getHTTPRequestErrorParams(error!)
+            try! respond(client, payload: "HTTP/1.1 \(code) \(message)")
+          }
+
+          try! respond(client, payload: "Welcome on Octopus, Please setup a new Router")
+
+          release(client.fileDescriptor)
+
+          sync (self) {
+            self.clients.remove(client)
+          }
+        }
+      }
+    }
+  }
+
+  public func stop() {
+    release(self.socket.fileDescriptor)
+
+    sync (self) {
+      for client in self.clients {
+        shutdownSocket(client.fileDescriptor)
+      }
+
+      self.clients.removeAll(keepCapacity: true)
+    }
+  }
+}
+
+func respond(socket: OctopusSocket, payload: String = "") throws {
+  let payloadAsBytes: [UInt8] = Array(payload.utf8)
+
+  try writeSocket(socket, string: "HTTP/1.1 200 OK\r\n")
+  try writeSocket(socket, string: "Server: Octopus\n")
+  try writeSocket(socket, string: "Content-Length: \(payloadAsBytes.count ?? 0)\r\n")
+  try writeSocket(socket, string: "Content-type: text-plain\n")
+  try writeSocket(socket, string: "\r\n")
+  try writeSocket(socket, string: payload)
+}
