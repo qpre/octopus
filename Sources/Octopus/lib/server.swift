8023d59222b64e9e1fe523c8d71922a6098fb1db
Quentin Pre <pre.quentin@gmail.com>
2015-12-14 00:53:56 +0100

    [ADD] protecting payload

--- Sources/Octopus/lib/server.swift
+++ Sources/Octopus/lib/server.swift
@@ -91,14 +91,16 @@ public class OctopusServer {
 func respond(socket: OctopusSocket, payload: String = "") throws {
   print("responding to client")
 
-  let payloadAsBytes: [UInt8] = Array(payload.utf8)
-
-  try writeSocket(socket, string: "HTTP/1.1 200 OK\r\n")
-  try writeSocket(socket, string: "Server: Octopus\n")
-  try writeSocket(socket, string: "Content-Length: \(payloadAsBytes.count ?? 0)\r\n")
-  try writeSocket(socket, string: "Content-type: text-plain\n")
-  try writeSocket(socket, string: "\r\n")
-  try writeSocket(socket, string: payload)
+  do {
+    try writeSocket(socket, string: "HTTP/1.1 200 OK\r\n")
+    try writeSocket(socket, string: "Server: Octopus\n")
+    try writeSocket(socket, string: "Content-Length: \(payload.characters.count)\r\n")
+    try writeSocket(socket, string: "Content-type: text-plain\n")
+    try writeSocket(socket, string: "\r\n")
+    try writeSocket(socket, string: payload)
+  } catch {
+    print("Failed at sending payload")
+  }
 
   print("response sent to client")
 }
